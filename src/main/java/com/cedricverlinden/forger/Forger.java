93a3f0e52fe77b8cd7fe2d6749120a9185559bcc
Cédric <work@cedricverlinden.com>
2023-07-14 02:59:44 +0200

    Removed debug command

--- src/main/java/com/cedricverlinden/forger/Forger.java
+++ src/main/java/com/cedricverlinden/forger/Forger.java
@@ -4,7 +4,6 @@ import java.util.Objects;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.cedricverlinden.forger.commands.DebugCommand;
 import com.cedricverlinden.forger.commands.ForgerCommand;
 import com.cedricverlinden.forger.commands.ItemCommand;
 import com.cedricverlinden.forger.commands.RecipeCommand;
@@ -45,7 +44,6 @@ public final class Forger extends JavaPlugin {
 	private void registerCommands() {
 		log.info("Registering commands...");
 		Objects.requireNonNull(getCommand("forger")).setExecutor(new ForgerCommand());
-		Objects.requireNonNull(getCommand("debug")).setExecutor(new DebugCommand());
 		Objects.requireNonNull(getCommand("item")).setExecutor(new ItemCommand());
 		Objects.requireNonNull(getCommand("recipe")).setExecutor(new RecipeCommand());
 		log.info("Commands registered.");
